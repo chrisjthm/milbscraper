3bf38fae8fd5434dec053e252d829ca2186550cb
Christopher J Monsen <chrismonsen@Christophers-MacBook-Pro.local>
2018-05-14 20:14:03 -0400

    fix deployment

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,22 @@ FROM python:2.7-slim
 RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
 RUN apt-get install -y git
-RUN apt-get install -y wget
+RUN apt-get install -y wget xvfb unzip
 # install manually all the missing libraries
 RUN apt-get install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+
+# Set up Chromedriver Environment variables
+ENV CHROMEDRIVER_VERSION 2.37
+ENV CHROMEDRIVER_DIR /chromedriver
+RUN mkdir $CHROMEDRIVER_DIR
+
+# Download and install Chromedriver
+RUN wget -q --continue -P $CHROMEDRIVER_DIR "http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+RUN unzip $CHROMEDRIVER_DIR/chromedriver* -d $CHROMEDRIVER_DIR
+RUN chmod +x $CHROMEDRIVER_DIR/chromedriver*
+
+ENV PATH $CHROMEDRIVER_DIR/chromedriver:$PATH
+
 
 # install chrome
 RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -13,6 +26,7 @@ copy . /app
 WORKDIR /app
 RUN pip install flask
 RUN pip install selenium
+RUN pip install pyvirtualdisplay
 EXPOSE 5000
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- playerpagecollector.py
+++ playerpagecollector.py
@@ -4,13 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
+from pyvirtualdisplay import Display
 
 
 class PlayerPageCollector:
 
-	PLAYER_IDS = {"VLADIMIRGUERREROJR":665489, "FORRESTWHITLEY":666215}
+	PLAYER_IDS = {"VLADIMIRGUERREROJR":665489, "KESTONHIURA":669374, "LUISURIAS":649966}
 	base_url = 'http://www.milb.com/player/index.jsp'
-	chromedriver_path = os.getcwd() + '/chromedriver_linux'
+	chromedriver_path = '/chromedriver/chromedriver'
 
 	def add_player_id(self, lastname, firstname, id):
 		player_name_key = self.get_player_name_key(lastname, firstname)
@@ -28,10 +29,14 @@ class PlayerPageCollector:
 		return self.base_url + '?player_id=' + str(player_id)
 
 	def setupBrowser(self):
-		option = webdriver.ChromeOptions()
-		option.add_argument(" - incognito")
+		options = webdriver.ChromeOptions()
+		options.add_argument(" - incognito")
+		options.add_argument('--disable-extensions')
+		options.add_argument('--headless')
+		options.add_argument('--disable-gpu')
+		options.add_argument('--no-sandbox')
 
-		browser = webdriver.Chrome(executable_path=self.chromedriver_path, chrome_options=option)
+		browser = webdriver.Chrome(executable_path=self.chromedriver_path, chrome_options=options)
 		return browser
 
 	def get_player_page(self, url) :
@@ -53,10 +58,13 @@ class PlayerPageCollector:
 		return stats_table[0].text.splitlines()
 
 	def get_latest_game_data(self, url):
+		display = Display(visible=0, size=(800, 600))
+		display.start()
 		stats_arr = self.get_stats_table_data(url)
 		headers = stats_arr[0].split(' ')
 		data = stats_arr[-2].split(' ')
 		latest_game_data = {}
 		for i in list(range(len(data) - 1)):
 			latest_game_data[headers[i]] = data[i]
+		display.stop()
 		return latest_game_data	
